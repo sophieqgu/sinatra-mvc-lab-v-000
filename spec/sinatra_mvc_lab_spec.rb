4cfed7b9c43eed2ecadbed7d9f87e45ef9106b5d
Sophie Gu <sophieqgu@gmail.com>
2019-09-04 03:08:06 +0000

    Automatically backed up by Learn

--- spec/sinatra_mvc_lab_spec.rb
+++ spec/sinatra_mvc_lab_spec.rb
@@ -40,7 +40,7 @@ describe "Pig Latinizer App" do
     end
 
     it "displays the pig latinized phrase upon form submission" do
-      #expect(last_response.body).to include("Onceway uponway away imetay andway away eryvay oodgay imetay itway asway erethay asway away oocowmay omingcay ownday alongway ethay oadray andway isthay oocowmay atthay asway omingcay ownday alongway ethay oadray etmay away icenay ittlelay oybay amednay abybay uckootay")
+      expect(last_response.body).to include("Onceway uponway away imetay andway away eryvay oodgay imetay itway asway erethay asway away oocowmay omingcay ownday alongway ethay oadray andway isthay oocowmay atthay asway omingcay ownday alongway ethay oadray etmay away icenay ittlelay oybay amednay abybay uckootay")
     end
   end
 
